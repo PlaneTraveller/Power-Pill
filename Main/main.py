a1724ff03053d5a942bba62ae788f61359d0a286
Huayu Wang <why123jj@126.com>
2021-05-25 14:13:42 +0800

    Update: Project Demo

--- Main/main.py
+++ Main/main.py
@@ -1,4 +1,16 @@
-import requests
+import requests as req
 import json
+import pandas as pd
 
-# Obtaining website URL
+#=== Pending: Obtaining website URL
+
+#=== Requesting data
+
+#--- Demo: CA, raw positive
+reqResp = req.get(
+    'https://jhucoronavirus.azureedge.net/api/v1/timeseries/us/cases/CA.json')
+datDic = reqResp.json()
+datDF = pd.DataFrame.from_dict(datDic, orient='index')
+datDF.to_csv('./Main/output/DemoOut.csv')
+
+#--- Demo Finish
