62684ec972ffc01aa60e31ebcfcfe9192bf71e27
Mograine <907881557@qq.com>
2021-05-28 22:44:14 +0800

    fix:small changes;build:some basic data

--- Main/another_hospitalization.py
+++ Main/another_hospitalization.py
@@ -1,13 +1,17 @@
-#hospitalization for all states which, compared with the previous one, show earlier but less detailed data
 import requests 
 import pandas
 import json
-with open('C:/Users/86183/Documents/Tencent Files/907881557/FileRecv/state_name.json') as state:    #read in the state name file
-    state=json.load(state)  #change the .json into a dict
-for key in state:   #go through every element
-    hospitalization=requests.get('https://jhucoronavirus.azureedge.net/api/v1/timeseries/us/hospitalization/{}.json'.format(key))   #fill the .json name automatically
-    hospitalization=hospitalization.json()  #change the .json into a list whose element number is one
-    hospitalization=hospitalization[0]  #change the list into a dict
-    data=pandas.DataFrame.from_dict(hospitalization,orient=('index'))   #change the dict into .csv
-    data.to_csv('C:/Git/Power-Pill/Main/output/{}_hospitalization.csv'.format(state[key])) #generate a .csv whose name can be filled with the state name automatically
-#'https://jhucoronavirus.azureedge.net/jhucoronavirus/testing/jh-covid-tool.v3.json'(TESTING OVERVIEW). Perhaps necessary
+
+with open('C:/Users/86183/Documents/Tencent Files/907881557/FileRecv/state_name.json') as state:
+    state=json.load(state)
+# read in the state name file and change the .json into a dict
+
+for key in state:
+    hospitalization=requests.get('https://jhucoronavirus.azureedge.net/api/v1/timeseries/us/hospitalization/{}.json'.format(key))
+    hospitalization=hospitalization.json()
+    hospitalization=hospitalization[0]
+# change the .json into a dict
+
+    data=pandas.DataFrame.from_dict(hospitalization,orient=('index'))
+    data.to_csv('C:/Git/Power-Pill/Main/output/{}_hospitalization.csv'.format(state[key]))
+# generate a .csv whose name can be filled with the state name automatically
--- Main/vaccinations.py
+++ Main/vaccinations.py
@@ -1,15 +1,28 @@
 import requests 
 import pandas
 import json
-with open('C:/Users/86183/Documents/Tencent Files/907881557/FileRecv/state_name.json') as state:    #read in the state name file
-    state=json.load(state)  #change the .json into a dict
-for key in state:   #go through every element(state)
-    vaccinations=requests.get('https://jhucoronavirus.azureedge.net/api/v1/timeseries/us/vaccines/{}.json'.format(key))   #fill the .json name automatically
-    vaccinations=vaccinations.json()  #change the .json into a list
-    data=pandas.DataFrame({},index=['doses_admin_daily','7_day_avg'])   #summon an empty dataframe
-    for i in range(0,len(vaccinations)):    #go through every element(date)
-        v=vaccinations[i]   #pick out an element(dict)
-        columns=pandas.DataFrame.from_dict(v,orient=('index'))  #change the dict into a column of a datafrane
-        data[v['date']]=columns #add the column to the dataframe
-    output=pandas.DataFrame(data,index=['doses_admin_daily','7_day_avg'])   #change the dict into a .csv
-    output.to_csv('C:/Git/Power-Pill/Main/output/{}_vaccinations.csv'.format(state[key])) #generate a .csv whose name can be filled with the state name automatically
+
+with open('C:/Users/86183/Documents/Tencent Files/907881557/FileRecv/state_name.json') as state:
+    state=json.load(state)
+# read in the state name file and change the .json into a dict
+
+for key in state:
+    vaccinations=requests.get('https://jhucoronavirus.azureedge.net/api/v1/timeseries/us/vaccines/{}.json'.format(key))
+    vaccinations=vaccinations.json()
+# change the .json into a list
+
+    data=pandas.DataFrame({},index=['doses_admin_daily','7_day_avg'])
+# summon an empty dataframe
+
+    for i in range(0,len(vaccinations)):
+        v=vaccinations[i]
+# every element, in form of a dict, in the list is the data for a certain date
+
+        columns=pandas.DataFrame.from_dict(v,orient=('index'))
+        data[v['date']]=columns
+# add the column to the dataframe
+
+    output=pandas.DataFrame(data)
+    output=output.T
+    output.to_csv('C:/Git/Power-Pill/Main/output/{}_vaccinations.csv'.format(state[key]))
+# generate a .csv whose name can be filled with the state name automatically
