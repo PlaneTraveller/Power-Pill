8f40490ee398963a7ec6e7bbdfa77d07ec2999ca
DDaname <dinghan20011229@qq.com>
2021-06-05 16:25:28 +0800

    Update: binary

--- Main/raspberry.py
+++ Main/raspberry.py
@@ -1,7 +1,9 @@
 import json
 import csv
+'''
 from gpiozero import LED
 from time import sleep
+'''
 
 with open('../Main/input/state_name.json') as fl:
     stateStr = fl.read()
@@ -11,14 +13,18 @@ stateDict = json.loads(stateStr)
 with open('../Main/oldOut/California.csv') as fl:
     CARaw = csv.reader(fl)
     DatList = []
+    check = True
     for row in CARaw:
-        DatList.append(row[1])
+        if check == True:
+            check = False
+            continue
+        DatList.append(int(float(row[1])))
 DatList.pop(0)
 
+'''
+# basic code
 blue = LED(19)
 red = LED(26)
-
-# basic code
 for i in range(len(DatList)):
     if DatList[i + 1] > DatList[i]:
         red.on()
@@ -27,5 +33,23 @@ for i in range(len(DatList)):
         sleep(1)
     else:
         sleep(2)
+'''
 
 # bonus code
+RevList = []
+for i in range(len(DatList)):
+    RevList.append(DatList[len(DatList) - i - 1])
+
+BinList= []
+for Rev in RevList:
+    Bin = bin(Rev)
+    Bin = Bin[2:]
+    BinList.append(Bin)
+
+big = '0'
+for bin in BinList:
+    if len(bin) > len(big):
+        big = bin
+print(len(big))
+
+
