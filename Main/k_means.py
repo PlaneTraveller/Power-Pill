69434e55dcd5b0fe0e4e1463dd980b1f6f572606
DDaname <dinghan20011229@qq.com>
2021-06-09 20:16:38 +0800

    Update: k_means part

---
+++ Main/k_means.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from copy import deepcopy
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+plt.rcParams['figure.figsize'] = (16, 9)
+plt.style.use('ggplot')
